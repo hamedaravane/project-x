1e277268484b5090bee2152a1b4e138e95c387f8
hamed <h.arghavan@ompfinex.com>
2023-09-10 21:40:55 +0330

    some improvement for select-profile-photo.component.ts

---
+++ src/app/authentication/feature/register/data-access/cropper.service.ts
@@ -0,0 +1,19 @@
+import {ElementRef, Injectable} from '@angular/core';
+import Cropper from 'cropperjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CropperService {
+
+  /**
+   * Initializes a Cropper instance on the given image element with the specified options.
+   *
+   * @param {ElementRef} imageElement - The reference to the image DOM element.
+   * @param {object} options - Cropper initialization options.
+   * @returns {Cropper} - Returns the initialized Cropper instance.
+   */
+  initCropper(imageElement: ElementRef, options: any): Cropper {
+    return new Cropper(imageElement.nativeElement, options);
+  }
+}
--- src/app/authentication/feature/register/select-profile-photo/select-profile-photo.component.ts
+++ src/app/authentication/feature/register/select-profile-photo/select-profile-photo.component.ts
@@ -1,10 +1,12 @@
-import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, inject, OnDestroy, ViewChild} from '@angular/core';
 import {NzUploadModule} from "ng-zorro-antd/upload";
 import {NzButtonModule} from "ng-zorro-antd/button";
 import Cropper from 'cropperjs';
 import {NzModalModule} from "ng-zorro-antd/modal";
-import {NgIf} from "@angular/common";
+import {NgClass, NgIf} from "@angular/common";
 import {RouterLink} from "@angular/router";
+import {ImageCompressionService} from "../data-access/image-compression.service";
+import {CropperService} from "../data-access/cropper.service";
 
 @Component({
   standalone: true,
@@ -15,21 +17,35 @@ import {RouterLink} from "@angular/router";
     NzButtonModule,
     NzModalModule,
     NgIf,
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   styleUrls: ['./select-profile-photo.component.scss']
 })
 export class SelectProfilePhotoComponent implements AfterViewInit, OnDestroy {
+  private readonly imageCompressionService = inject(ImageCompressionService);
+  private readonly cropperService = inject(CropperService);
+
   @ViewChild('fileInput') fileInput!: ElementRef;
   @ViewChild('image') imageElement!: ElementRef;
+  compressedFile!: File;
   selectedImageSrc: string | null = null;
+  croppedImageSrc: string | null = null;
   isCropModalVisible = false;
   cropper!: Cropper;
 
+  /**
+   * Programmatically triggers the file input to open the file selection dialog.
+   */
   triggerFileInput() {
     this.fileInput.nativeElement.click();
   }
 
+  /**
+   * Handles the event when a file is selected. Compresses and prepares the image for cropping.
+   *
+   * @param {Event} event - The file selection event.
+   */
   onFileSelected(event: Event) {
     const input: HTMLInputElement = event.target as HTMLInputElement;
 
@@ -37,46 +53,72 @@ export class SelectProfilePhotoComponent implements AfterViewInit, OnDestroy {
       return;
     }
 
-    const file = input.files[0];
-    this.selectedImageSrc = URL.createObjectURL(file);
-    this.isCropModalVisible = true;
-
-    setTimeout(() => {
-      this.initCropper();
+    this.imageCompressionService.compressImage(input.files[0]).then((f) => {
+      this.compressedFile = f;
+      this.selectedImageSrc = URL.createObjectURL(this.compressedFile);
+      this.isCropModalVisible = true;
+      setTimeout(() => {
+        this.initCropper();
+      });
     });
   }
 
+  /**
+   * Initializes or re-initializes the Cropper instance on the image element.
+   */
   initCropper() {
     if (this.cropper) {
       this.cropper.destroy();
     }
 
-    this.cropper = new Cropper(this.imageElement.nativeElement, {
-      aspectRatio: 1,
-    });
+    const options = { aspectRatio: 1 };
+    this.cropper = this.cropperService.initCropper(this.imageElement, options);
   }
 
-  closeCropModal() {
+  /**
+   * Closes the cropping modal and resets any selected or cropped images.
+   */
+  cancelCropModal() {
+    this.selectedImageSrc = null;
+    this.croppedImageSrc = null;
     this.isCropModalVisible = false;
   }
 
+  /**
+   * Confirms the image cropping, generates the data URL of the cropped image, and closes the cropping modal.
+   */
   confirmCrop() {
     const canvas = this.cropper.getCroppedCanvas();
 
     if (canvas) {
-      this.selectedImageSrc = canvas.toDataURL();
+      this.selectedImageSrc = null;
+      this.croppedImageSrc = canvas.toDataURL();
     }
 
     this.cropper.destroy();
     this.isCropModalVisible = false;
   }
 
+  /**
+   * Clear the selected Image
+   */
+  clearSelectedImage() {
+    this.selectedImageSrc = null;
+    this.croppedImageSrc = null;
+  }
+
+  /**
+   * After view initialization, sets up the Cropper if a cropped image is already available.
+   */
   ngAfterViewInit() {
-    if (this.selectedImageSrc) {
+    if (this.croppedImageSrc) {
       this.initCropper();
     }
   }
 
+  /**
+   * Cleanup method to destroy the Cropper instance before the component is destroyed.
+   */
   ngOnDestroy() {
     if (this.cropper) {
       this.cropper.destroy();
