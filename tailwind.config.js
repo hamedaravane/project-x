f8882eb67b217e99f9d944ec0d737c5c2abdc61a
Hamed <hamedaravane@gmail.com>
2023-09-29 10:05:21 +0330

    prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       lineHeight: {
-        'unset': 'unset'
-      }
+        unset: 'unset',
+      },
     },
   },
   plugins: [],
-}
-
+};
