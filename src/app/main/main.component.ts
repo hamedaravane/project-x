fe7c14104265fd1d898e427f84ba81276e612a42
hamed <h.arghavan@ompfinex.com>
2023-10-10 19:16:38 +0330

    format the src

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,6 @@
-import { PageHeaderComponent } from '@shared/ui/page-header/page-header.component';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import {PageHeaderComponent} from '@shared/ui/page-header/page-header.component';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   standalone: true,
