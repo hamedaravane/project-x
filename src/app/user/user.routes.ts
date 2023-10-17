afbdd6b44e3f8b170db85dc052b8db685aaedba8
hamed <h.arghavan@ompfinex.com>
2023-10-17 14:05:19 +0330

    edit-information component

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -3,7 +3,18 @@ import {Routes} from '@angular/router';
 export const userRoutes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./feature/edit-information/edit-information.component').then(c => c.EditInformationComponent),
+    loadComponent: () => import('./root/root.component').then(c => c.RootComponent),
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'edit',
+      },
+      {
+        path: 'edit',
+        loadComponent: () =>
+          import('./feature/edit-information/edit-information.component').then(c => c.EditInformationComponent),
+      },
+    ],
   },
 ];
