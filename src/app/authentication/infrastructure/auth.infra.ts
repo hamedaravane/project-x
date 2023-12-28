e16d24c3ddeaabf8daa6e6856d88e157df49c8e6
Hamed <hamedaravane@gmail.com>
2023-12-28 13:35:20 +0330

    define env and provide it in auth infra

--- src/app/authentication/infrastructure/auth.infra.ts
+++ src/app/authentication/infrastructure/auth.infra.ts
@@ -1,5 +1,5 @@
 import {HttpClient} from '@angular/common/http';
-import {Injectable, inject} from '@angular/core';
+import {Inject, Injectable, inject} from '@angular/core';
 import {ApiResponse} from '@shared/data-access/models/api-response.model';
 import {Observable, map} from 'rxjs';
 
@@ -7,10 +7,10 @@ import {Observable, map} from 'rxjs';
   providedIn: 'root',
 })
 export class AuthInfra {
+  constructor(@Inject('API_URL') private apiUrl: string) {}
   private readonly http = inject(HttpClient);
-
   register(data: any): Observable<ApiResponse<void>> {
-    return this.http.post<ApiResponse<void>>('', data).pipe(
+    return this.http.post<ApiResponse<void>>(`${this.apiUrl}/user/register`, data).pipe(
       map(res => {
         return res;
       }),
