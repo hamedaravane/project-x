229c029af80ee9e08311ea881513f2972bb967c1
Hamed Arghavan <hamedaravane@gmail.com>
2024-01-01 09:56:27 +0330

    change some typing

--- src/app/authentication/infrastructure/auth.infra.ts
+++ src/app/authentication/infrastructure/auth.infra.ts
@@ -1,6 +1,6 @@
 import {HttpClient} from '@angular/common/http';
 import {Inject, Injectable, inject} from '@angular/core';
-import {UserEntity} from '@user/data-access/model/user.model';
+import {CreateUserDto} from '@user/data-access/model/user.model';
 import {BaseInfraService} from '@shared/data-access/base-infra.service';
 import {ApiResponse} from '@shared/data-access/models/api-response.model';
 import {Observable, map} from 'rxjs';
@@ -16,13 +16,13 @@ export class AuthInfra extends BaseInfraService {
 
   /**
    * @description send registration form to backend.
-   * @param {UserEntity} data - user register form.
+   * @param {CreateUserDto} data - user register form.
    * @returns {ApiResponse<void>} - void.
    * @author Hamed Arghavan
    * @example
    * this._authInfra.register(data);
    */
-  register(data: UserEntity): Observable<ApiResponse<void>> {
+  register(data: CreateUserDto): Observable<ApiResponse<void>> {
     console.log('register post to the server...');
     return this.http.post<ApiResponse<void>>(`${this.apiUrl}/users/create`, data).pipe(
       map(res => {
