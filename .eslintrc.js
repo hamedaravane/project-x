3ff902479478567c12f5ecbd11b1f83ab667889f
hamed <h.arghavan@ompfinex.com>
2024-01-28 19:30:07 +0330

    edit eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,7 +47,7 @@ module.exports = {
       rules: {
         "@angular-eslint/template/interactive-supports-focus": ["off"],
         "@angular-eslint/template/click-events-have-key-events": ["off"],
-        "prettier": [
+        "prettier/prettier": [
           "error",
           {
             "parser": "angular"
