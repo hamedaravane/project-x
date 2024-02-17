c9f4f8a60a55913da9242e609c08ec72be8bc0b4
Hamed Arghavan <hamedaravane@gmail.com>
2024-02-17 21:28:18 +0330

    update auth token

--- src/app/authentication/data-access/model/auth.model.ts
+++ src/app/authentication/data-access/model/auth.model.ts
@@ -1,3 +1,5 @@
+import {UserDto} from '@user/data-access/model/user.model';
+
 export interface LoginEntity {
   email: string,
   password: string,
@@ -7,4 +9,12 @@ export interface LoginEntity {
 export interface LoginResponse {
   message: string;
   token: string;
+  user: UserDto;
+}
+
+export function convertToLoginRes(data: LoginResponse): LoginResponse {
+  return {
+    message: data.message,
+    token: data.token,
+  };
 }
