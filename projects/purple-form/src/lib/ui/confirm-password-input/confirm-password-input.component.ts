298a03a6c3c1e70766047bac348081e077caf706
Hamed Arghavan <hamedaravane@gmail.com>
2024-02-27 00:08:40 +0330

    value accessor

--- projects/purple-form/src/lib/ui/confirm-password-input/confirm-password-input.component.ts
+++ projects/purple-form/src/lib/ui/confirm-password-input/confirm-password-input.component.ts
@@ -1,11 +1,10 @@
-import {Component, Input} from '@angular/core';
-import {NzInputDirective, NzInputGroupComponent, NzInputGroupWhitSuffixOrPrefixDirective} from 'ng-zorro-antd/input';
-import {NzFormControlComponent, NzFormItemComponent} from 'ng-zorro-antd/form';
 import {NgClass, NgIf} from '@angular/common';
+import {Component, Input} from '@angular/core';
 import {AbstractControl, ReactiveFormsModule} from '@angular/forms';
+import {NzFormControlComponent, NzFormItemComponent} from 'ng-zorro-antd/form';
 import {NzColDirective} from 'ng-zorro-antd/grid';
+import {NzInputDirective, NzInputGroupComponent, NzInputGroupWhitSuffixOrPrefixDirective} from 'ng-zorro-antd/input';
 import {NzPopoverDirective} from 'ng-zorro-antd/popover';
-import {NzTransitionPatchDirective} from 'ng-zorro-antd/core/transition-patch/transition-patch.directive';
 
 @Component({
   templateUrl: 'confirm-password-input.component.html',
@@ -19,7 +18,6 @@ import {NzTransitionPatchDirective} from 'ng-zorro-antd/core/transition-patch/tr
     NzColDirective,
     NzInputGroupWhitSuffixOrPrefixDirective,
     NzPopoverDirective,
-    NzTransitionPatchDirective,
     ReactiveFormsModule,
     NgClass,
   ],
--- projects/purple-form/src/lib/ui/email-input/email-input.component.ts
+++ projects/purple-form/src/lib/ui/email-input/email-input.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input} from '@angular/core';
-import {NzInputDirective, NzInputGroupComponent} from 'ng-zorro-antd/input';
-import {NzFormControlComponent, NzFormItemComponent} from 'ng-zorro-antd/form';
 import {NgIf} from '@angular/common';
-import {AbstractControl, ReactiveFormsModule} from '@angular/forms';
+import {Component, Input} from '@angular/core';
+import {AbstractControl, ControlValueAccessor, ReactiveFormsModule} from '@angular/forms';
+import {NzFormControlComponent, NzFormItemComponent} from 'ng-zorro-antd/form';
+import {NzInputDirective, NzInputGroupComponent} from 'ng-zorro-antd/input';
+import {DEFAULT_VALUE_ACCESSOR} from '../value-accessor';
 
 @Component({
   templateUrl: 'email-input.component.html',
@@ -16,7 +17,19 @@ import {AbstractControl, ReactiveFormsModule} from '@angular/forms';
     ReactiveFormsModule,
   ],
   selector: 'purple-form-email-input',
+  providers: [DEFAULT_VALUE_ACCESSOR],
 })
-export class EmailInputComponent {
-  @Input({required: true}) emailControl!: AbstractControl;
+export class EmailInputComponent implements ControlValueAccessor {
+  writeValue(obj: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
 }
